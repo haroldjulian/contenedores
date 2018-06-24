1425983b75c897cc936131a0d1d1652c03ea1ce2
user-PC\user <araujoobeso_mj@hotmail.com>
2018-06-24 15:41:42 -0500

    Initial commit

---
+++ app/src/main/java/aplicacioncontroles/contenedores/LinearDemo.java
@@ -0,0 +1,13 @@
+package aplicacioncontroles.contenedores;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LinearDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_demo);
+    }
+}
---
+++ app/src/main/java/aplicacioncontroles/contenedores/RelativeDemo.java
@@ -0,0 +1,13 @@
+package aplicacioncontroles.contenedores;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RelativeDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_demo);
+    }
+}
